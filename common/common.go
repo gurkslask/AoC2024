78edf39ccd323bc831d7223ab824aeb49fc9db47
gurkslask <gurkslask@gmail.com>
2024-12-01 19:21:44 +0100

    Add comments to common lib

--- common/common.go
+++ common/common.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 )
 
+// Takes a slice of ints and returns sum
 func SumInts(ints []int) int {
 	var result int
 	for _, val := range ints {
@@ -15,6 +16,7 @@ func SumInts(ints []int) int {
 	return result
 }
 
+// Compares two ints and returns the lowest
 func MinInt(i, j int) int {
 	if i < j {
 		return i
@@ -22,6 +24,8 @@ func MinInt(i, j int) int {
 		return j
 	}
 }
+
+// Compares two ints and returns the highest
 func MaxInt(i, j int) int {
 	if i > j {
 		return i
@@ -30,6 +34,7 @@ func MaxInt(i, j int) int {
 	}
 }
 
+// Takes a slice of ints and returns the lowest value and the corresponding key
 func MinInts(ints []int) (int, int) {
 	result := ints[0]
 	rKey := 0
@@ -41,22 +46,28 @@ func MinInts(ints []int) (int, int) {
 	}
 	return result, rKey
 }
-func MaxInts(ints []int) int {
+
+// Takes a slice of ints and returns the highest value and the corresponding key
+func MaxInts(ints []int) (int, int) {
 	result := ints[0]
-	for _, val := range ints {
+	rKey := 0
+	for key, val := range ints {
 		if val > result {
 			result = val
+			rKey = key
 		}
 	}
-	return result
+	return result, rKey
 }
 
+// Check if error, if error print it
 func CheckErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
+// Takes the current day "dayX" as a string, and if test it checks the file "test_input.txt", returns a slice of string with the content of the file
 func ReadFileSlice(day string, test bool) []string {
 	fmt.Println(os.Getwd())
 	path := ""
@@ -73,6 +84,8 @@ func ReadFileSlice(day string, test bool) []string {
 
 	return s[:len(s)-1]
 }
+
+// Checks slice of string if it contains string i, return True of False
 func ContainsString(slice []string, i string) bool {
 	res := false
 	for _, num := range slice {
@@ -85,6 +98,7 @@ func ContainsString(slice []string, i string) bool {
 
 }
 
+// Checks slice of int if it contains int i, return True of False
 func ContainsInt(slice []int, i int) bool {
 	res := false
 	for _, num := range slice {
@@ -97,12 +111,17 @@ func ContainsInt(slice []int, i int) bool {
 
 }
 
+// Converts string s to int, returns int
+// Removes trailing whitespace
 func StrToInt(s string) int {
 	s = strings.Replace(s, " ", "", -1)
 	i, err := strconv.Atoi(s)
 	CheckErr(err)
 	return i
 }
+
+// Converts slice of string to slice of int
+// If only white space, ignore
 func StringsToInt(s []string) []int {
 	res := []int{}
 	for _, v := range s {
@@ -113,18 +132,23 @@ func StringsToInt(s []string) []int {
 	}
 	return res
 }
+
+// Makes absolute value of int
 func IntAbs(i int) int {
 	if i < 0 {
 		i = -i
 	}
 	return i
 }
+
+// Convert string to int64
 func StrToInt64(s string) int64 {
 	i, err := strconv.Atoi(s)
 	CheckErr(err)
 	return int64(i)
 }
 
+// Converts a slice of strings to a slice of ints
 func ConvertSliceStringToInt(slice []string) []int {
 	si := []int{}
 	fmt.Printf("slice = %+v\n", slice)
@@ -135,6 +159,8 @@ func ConvertSliceStringToInt(slice []string) []int {
 	}
 	return si
 }
+
+// Converts a slice of strings to a slive of int64
 func ConvertSliceStringToInt64(slice []string) []int64 {
 	si := []int64{}
 	fmt.Printf("slice = %+v\n", slice)
@@ -146,6 +172,8 @@ func ConvertSliceStringToInt64(slice []string) []int64 {
 	return si
 }
 
+// A map with numbers matched to english words
+// Returns map with key as the string and value int
 func mapNumtextAndNums() map[string]int {
 	numbers := map[string]int{
 		"one":   1,
@@ -160,13 +188,16 @@ func mapNumtextAndNums() map[string]int {
 		"zero":  0,
 	}
 	return numbers
-
 }
+
+// A map where the alphabet is mapped to incremental numbers
+// Returns map with key as the string and value int
 func mapAlphaWithNum() map[string]int {
 	var A = map[string]int{"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26}
 	return A
 }
 
+// Check if string can be converted to int
 func CheckIfInt(s string) bool {
 	_, err := strconv.Atoi(s)
 	if err != nil {
@@ -176,6 +207,7 @@ func CheckIfInt(s string) bool {
 	}
 }
 
+// Check all the keys of the map if value exists
 func MapContainsKey(indata map[int]int, value int) bool {
 	if indata[value] == 0 {
 		return true
@@ -184,6 +216,7 @@ func MapContainsKey(indata map[int]int, value int) bool {
 	}
 }
 
+// Remove key i from slice
 func RemoveFromSliceInt(s []int, i int) []int {
 	s[i] = s[len(s)-1]
 	return s[:len(s)-1]
