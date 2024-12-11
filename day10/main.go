e5b734c0484fae16ec8184d2a47b58718b234cf3
gurkslask <gurkslask@gmail.com>
2024-12-11 15:33:15 +0100

    Advanced done

--- day10/main.go
+++ day10/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	f := common.ReadFileSlice("day10", false)
-	simple(f)
-	//adv(f)
+	//simple(f)
+	adv(f)
 
 }
 
@@ -54,70 +54,96 @@ func check(g common.Grid, row int, col int, taken []common.Pos) (int, []common.P
 	}
 	//Left
 	if StringsToIntwithO(g.CheckLeft(row, col)) == curloc+1 {
-		/*if curloc == 8 && !CheckIfPosTaken(taken, row, col-1) {
-			PrintEnd(g, row, col)
-			res += 1
-			taken = append(taken, common.Pos{row, col - 1})
-			return res, taken
-		} elseif CheckIfPosTaken(taken, row, col-1) {
-			return res, taken
-		} else {
-		*/
 		tres := 0
 		tres, taken = check(g, row, col-1, taken)
 		res += tres
-		//}
 	}
 	//Right
 	if StringsToIntwithO(g.CheckRight(row, col)) == curloc+1 {
-		/*if curloc == 8 && !CheckIfPosTaken(taken, row, col+1) {
-			PrintEnd(g, row, col)
-			res += 1
-			taken = append(taken, common.Pos{row, col + 1})
-			return res, taken
-		} elseif CheckIfPosTaken(taken, row, col-1) {
-			return res, taken
-		} else {*/
 		tres := 0
 		tres, taken = check(g, row, col+1, taken)
 		res += tres
-		//}
 	}
 	//Up
 	if StringsToIntwithO(g.CheckUp(row, col)) == curloc+1 {
-		/*if curloc == 8 && !CheckIfPosTaken(taken, row-1, col-1) {
-			PrintEnd(g, row, col)
-			res += 1
-			taken = append(taken, common.Pos{row - 1, col})
-			return res, taken
-		} else if CheckIfPosTaken(taken, row, col-1) {
-			return res, taken
-		} else { */
 		tres := 0
 		tres, taken = check(g, row-1, col, taken)
 		res += tres
-		//}
 	}
 	//Down
 	if StringsToIntwithO(g.CheckDown(row, col)) == curloc+1 {
-		/*if curloc == 8 && !CheckIfPosTaken(taken, row+1, col) {
-			PrintEnd(g, row, col)
-			res += 1
-			taken = append(taken, common.Pos{row + 1, col})
-			return res, taken
-		} else if CheckIfPosTaken(taken, row, col-1) {
-			return res, taken
-		} else { */
 		tres := 0
 		tres, taken = check(g, row+1, col, taken)
 		res += tres
-		//}
 	}
 	//fmt.Printf("Found %v, res\n", res)
 	return res, taken
 }
 func adv(data []string) {
-	fmt.Printf("%b", data)
+	g := common.MakeGrid(len(data), len(data[0]))
+	for row, v := range data {
+		for col := range v {
+			s := string(data[row][col])
+			g.AddMarker(common.MakeMarker(row, col, s))
+		}
+	}
+	g.InitGrid()
+	g.Print()
+	res := 0
+
+	meh := make(map[common.Pos]int)
+	for row, v := range data {
+		for col := range v {
+			if g.CheckLoc(row, col) == "0" {
+				//fmt.Printf("Found 0, row: %v, col: %v\n", row, col)
+				_, meh = checkAdv(g, row, col, meh)
+
+				//fmt.Println(res)
+			}
+		}
+	}
+	fmt.Println(meh)
+	for _, v := range meh {
+		res += v
+	}
+	fmt.Println(res)
+
+}
+func checkAdv(g common.Grid, row int, col int, taken map[common.Pos]int) (int, map[common.Pos]int) {
+	res := 0
+	curloc := StringsToIntwithO(g.CheckLoc(row, col))
+	if curloc == 9 {
+		PrintEnd(g, row, col)
+		res += 1
+		taken[common.Pos{row, col}] += 1
+		return res, taken
+	}
+	//Left
+	if StringsToIntwithO(g.CheckLeft(row, col)) == curloc+1 {
+		tres := 0
+		tres, taken = checkAdv(g, row, col-1, taken)
+		res += tres
+	}
+	//Right
+	if StringsToIntwithO(g.CheckRight(row, col)) == curloc+1 {
+		tres := 0
+		tres, taken = checkAdv(g, row, col+1, taken)
+		res += tres
+	}
+	//Up
+	if StringsToIntwithO(g.CheckUp(row, col)) == curloc+1 {
+		tres := 0
+		tres, taken = checkAdv(g, row-1, col, taken)
+		res += tres
+	}
+	//Down
+	if StringsToIntwithO(g.CheckDown(row, col)) == curloc+1 {
+		tres := 0
+		tres, taken = checkAdv(g, row+1, col, taken)
+		res += tres
+	}
+	//fmt.Printf("Found %v, res\n", res)
+	return res, taken
 }
 func CheckIfPosTaken(t []common.Pos, row int, col int) bool {
 	res := false
